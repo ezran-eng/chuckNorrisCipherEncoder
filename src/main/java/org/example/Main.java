73358f5830c9aba33c0903981d1c5be0d5060f49
Ezequiel <eezquieloelero@gmail.com>
2023-05-03 19:18:46 -0300

    in process

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
-        System.out.println("Input a string:");
-        String inputString = scanner.nextLine();
-
-        StringBuilder outputString = new StringBuilder();
-        for (char c : inputString.toCharArray()) {
-            outputString.append(c).append(" ");
+        // Convert input to binary string
+        StringBuilder binaryString = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            String binary = Integer.toBinaryString(c);
+            // Pad with leading zeros to ensure 7-bit length
+            binaryString.append("0000000".substring(binary.length()) + binary);
         }
-        System.out.println(outputString.toString());
+
+        // Encode binary string using Chuck Norris Unary Code
+        StringBuilder encodedString = new StringBuilder();
+        int i = 0;
+        while (i < binaryString.length()) {
+            // Determine bit value and count of consecutive bits
+            char bit = binaryString.charAt(i);
+            int count = 1;
+            while (i < binaryString.length() - 1 && binaryString.charAt(i + 1) == bit) {
+                count++;
+                i++;
+            }
+            i++;
+
+            // Append encoded block to output string
+            encodedString.append(bit == '0' ? "00 " : "0 ");
+            for (int j = 0; j < count; j++) {
+                encodedString.append("0");
+            }
+            encodedString.append(" ");
+        }
+
+        // Print result
+        System.out.println("The result:");
+        System.out.println(encodedString.toString().trim());
     }
 }
